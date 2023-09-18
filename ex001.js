ed56dc6bb92de5f21a7b95fbffeee441c7915372
joao <joaodeus400@gmail.com>
2023-09-18 14:14:16 -0300

    exec

--- ex001.js
+++ ex001.js
@@ -20,3 +20,25 @@ console.log(media(40, 10))
 // const stringRepetida = stringOriginal.repeat(repeticoes)
 
 // console.log(stringRepetida) // Isso imprimirá 'Luis é corno. Luis é corno. Luis é corno. Luis
+
+
+var numeros = [1, 2, 3, 4, 5, 6, 7,8, 9, 10,11]; // como se estivessem somando os numeros 1 + 2 + 3.... e assim por diante
+var total = numeros.reduce((total, numero) => total + numero, 0);
+console.log(total);
+
+var numeroL = [6, 4, 10, 4]
+var totalL = numeroL.reduce((totalL, numeroL) => totalL + numeroL, 0)
+console.log(totalL)
+
+switch(totalL) {
+    case 24:
+         console.log('Ok')
+    break
+
+    case 30:
+        console.log('OK....');
+    break
+
+    default:
+        console.log('valor genérico');
+}
