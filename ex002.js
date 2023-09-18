7f5f35a160d1bc4d655a4ab8aabca326a17bcb3f
joao <joaodeus400@gmail.com>
2023-09-18 16:49:10 -0300

    fatorial

--- ex002.js
+++ ex002.js
@@ -1,6 +1,35 @@
+// MINHA RESOLUÇÃO
+
 function Chunk(string) {
   let result = (string + '-').repeat(4)  // repete-se 4 vezes, e ao final de cada string adiciona '-'// split separa a string 'chunk-chunk' por letra 'c', 'h', 'u' .... etc e join junta esse caracteres individuas no array, ficando 'chunk-chunk'
   console.log(result)
 }
 
 Chunk('chunk')
+
+// RESOLUÇÃO DA ONE-BIT-CODE
+
+function chunks(num) {
+    if (num == 0) {
+        return 'nada'
+    }
+
+    if (num === 1) {
+        return 'alo'
+    } else {
+        return 'hello-' + chunks(num -1)
+    }
+}
+
+console.log(chunks(4))
+
+
+// fatorial
+function fatorial(v) {
+    if (v == 0) {
+       return 1
+    } else {
+        return v - fatorial(v-1)
+    }
+}
+console.log("passou",fatorial(5))
